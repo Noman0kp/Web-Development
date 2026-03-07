f574b2d21ab06bd632389565688f3e222bac42cc
Noman0kp <nomankp1107@gmail.com>
2026-03-07 07:34:43 +0530

    Operators

---
+++ JavaScript/Operators.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b = 4;
+
+console.log(a + b); // ADDITION
+console.log(a - b); // SUBTRACTION
+console.log(a * b); // MULTIPLICATION
+console.log(a / b); // DIVISION
+console.log(a % b); // MODULUS (REMAINDER OF DIVISION)
+
+// INCREMENT AND DECREMENT OPERATORS
+// a++; INCREMENT a BY 1 (a = a + 1)
+// b--; DECREMENT b BY 1 (b = b - 1)    
+
+console.log(a**b); // EXPONENTIATION (a RAISED TO THE POWER OF b)
+
+
